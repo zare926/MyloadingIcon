e739c09c8e09db76dcb64673081f5045a99e3412
zare926 <ilove.boc926@gmail.com>
2020-06-30 07:16:12 +0900

    マークアップ準備とJS準備

---
+++ js/main.js
@@ -0,0 +1,19 @@
+'use strict'
+
+(() => {
+  class Icon {
+    constructor(canvas){
+
+    }
+    run(){
+      
+    }
+  }
+
+  const canvas = document.querySelector('canvas');
+  if (typeof canvas.getContext === 'undefined'){
+    return;
+  }
+  const icon = new icon(canvas)
+  icon.run();
+})();
